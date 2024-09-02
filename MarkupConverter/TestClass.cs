949ae3480e49c3260fed6846288a49fdbb63b92a
Albert Mosialek <amosialek@idexcorp.com>
2024-09-02 14:18:30 +0200

    test GH actions

---
+++ MarkupConverter/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkupConverter
+{
+    public class TestClass
+    {
+        static int i=0;
+        public int TestMethod()
+        {
+            for (i++; i % 3 != 0; i++) ;
+            return i;
+        }
+    }
+}
