eb1ee681b1ff84f1fc914c30a1ef9e5d4719f070
Albert Mosialek <amosialek@idexcorp.com>
2024-09-03 14:07:57 +0200

    add class which should not pass

--- MarkupConverter/TestCodeScanning.cs
+++ MarkupConverter/TestCodeScanning.cs
@@ -15,5 +15,15 @@ namespace MarkupConverter
             Console.WriteLine(i);
             return i;
         }
+
+    }
+}
+
+class Bad
+{
+    public static double SolveQuadratic(double a, double b, double c)
+    {
+        // TODO: handle case where a == 0
+        return (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
     }
 }
